c2ea6615c456e610a9b5d19c2d21084001f9b789
Dimas Pamungkas Mursidi <dmspmksmrsdi@gmail.com>
2023-12-27 19:37:37 +0700

    Update modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 // Modal.js
 import React, { useState } from "react";
-import {motion} from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion";
 
 const Modal = ({ dino, onClose }) => {
   const [modalVisible, setModalVisible] = useState(true);
@@ -10,7 +10,8 @@ const Modal = ({ dino, onClose }) => {
     onClose();
   };
 
-  const handleCloseClick = () => {
+  const handleCloseClick = (e) => {
+    e.stopPropagation();
     handleClose();
   };
 
@@ -38,34 +39,57 @@ const Modal = ({ dino, onClose }) => {
   };
 
   return (
-    <div className={`modal ${modalVisible ? "visible" : "hidden"}`} onClick={handleClose}>
-      <div className="overlay" onClick={handleClose}></div>
-      <div className="background"></div>
-      <motion.div
-        className="scrollable modal-content modal-overlay max-w-3xl bg-secondary text-white p-6 rounded-md"
-        onClick={handleCloseClick}
-        variants={dropIn}
-        initial="hidden"
-        animate="visible"
-        exit="exit"
-      >
-        <div className="flex flex-col-reverse gap-2 items-center mb-6">
-          <img src={image} alt={name} className="h-[200px] mr-6" />
-          <h3 className="font-logo w-10">{category}</h3>
-        </div>
-        <h3 className="text-lg font-logo -mt-4">{name}</h3>
-        <p className="py-4 text-justify indent-5 cursor-pointer">
-          {description}
-        </p>
-        <p className="-mt-2 text-justify indent-5">{secdescription}</p>
-        <button
-          className="btnPrimary cursor-pointer mt-1"
-          onClick={handleClose}
-        >
-          Close
-        </button>
-      </motion.div>
-    </div>
+    <AnimatePresence>
+      {modalVisible && (
+        <>
+          <motion.div
+            className="modal"
+            onClick={handleClose}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <motion.div
+              className="overlay"
+              onClick={handleClose}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            ></motion.div>
+            <motion.div
+              className="background"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            ></motion.div>
+            <motion.div
+              className="scrollable modal-content modal-overlay max-w-3xl bg-secondary text-white p-6 rounded-md"
+              onClick={handleCloseClick}
+              variants={dropIn}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <div className="flex flex-col-reverse gap-2 items-center mb-6">
+                <img src={image} alt={name} className="h-[200px] mr-6" />
+                <h3 className="font-logo w-10">{category}</h3>
+              </div>
+              <h3 className="text-lg font-logo -mt-4">{name}</h3>
+              <p className="py-4 text-justify indent-5 cursor-pointer">
+                {description}
+              </p>
+              <p className="-mt-2 text-justify indent-5">{secdescription}</p>
+              <button
+                className="btnPrimary cursor-pointer mt-1"
+                onClick={handleClose}
+              >
+                Close
+              </button>
+            </motion.div>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   );
 };
 
